777667b2952fa9f20fb4aaa1c289d11798478be9
Sergej <terehovsergej@gmail.com>
2015-03-23 12:35:17 +0200

    update bianki slicing

--- bianki/js/main.js
+++ bianki/js/main.js
@@ -4,12 +4,25 @@ $(document).ready(function() {
         slideshow: false,
         directionNav: false
     });
-
-
+    
     $('input, textarea').placeholder();
-
 
     $.scrollUp({
         scrollText: ''
     });
+});
+
+
+
+
+$(window).load(function() {
+    // banner slider
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        autoplay: 2000,
+        speed: 700,
+        effect: 'fade'
+    })    
+    // END banner slider
 });
